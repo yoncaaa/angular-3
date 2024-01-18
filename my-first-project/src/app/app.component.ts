2dace92b1f403f434fa8755e10aa1e250308edfd
yoncaaa <yoncaakceylan@web.de>
2024-01-18 16:01:29 +0100

    c

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component , OnInit , OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'my-first-project'; 
+  intervalSub;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() =>{
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }  
